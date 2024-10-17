a3518c8e99420b957e050ce5b6f3156b0180e2e3
+brioche <oscarwasilewski023@gmail.com>
2024-10-17 19:05:56 +0200

    Delete zoom.js

--- zoom.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.zoomable').forEach(img => {
-  img.addEventListener('click', function () {
-    if (this.style.transform === 'scale(3)') {
-      this.style.transform = 'scale(1)';
-    } else {
-      this.style.transform = 'scale(3)';
-    }
-  });
-});
